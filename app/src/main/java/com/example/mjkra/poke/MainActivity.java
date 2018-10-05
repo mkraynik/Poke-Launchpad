e4930dd90097db713e3a040bd69b49a9b02034ab
mkraynik <43658745+mkraynik@users.noreply.github.com>
2018-10-04 21:01:08 -0400

    Basic Notification Functionality

--- app/src/main/java/com/example/mjkra/poke/MainActivity.java
+++ app/src/main/java/com/example/mjkra/poke/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.mjkra.poke;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "default";
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello Wor");
+
+        createNotificationChannel();
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                EditText inputNum1 = (EditText) findViewById(R.id.inputNum1);
+                String str = inputNum1.getText().toString();
+                //Toast.makeText(getBaseContext(), str, Toast.LENGTH_SHORT).show();
+
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getBaseContext(), CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("My notification")
+                        .setContentText(str)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getBaseContext());
+
+                // notificationId is a unique int for each notification that you must define
+                notificationManager.notify(1, mBuilder.build());
+
+            }
+        });
     }
 }
