b9cc4107e98210c34d83fd7a34dd3346d2206c01
mkraynik <43658745+mkraynik@users.noreply.github.com>
2018-10-11 22:12:47 -0400

    Added Date Picker to change the text

--- app/src/main/java/com/example/mjkra/poke/MainActivity.java
+++ app/src/main/java/com/example/mjkra/poke/MainActivity.java
@@ -1,27 +1,37 @@
 package com.example.mjkra.poke;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private static final String CHANNEL_ID = "default";
+    public static TextView date;
+
     private void createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_HIGH);
             channel.setDescription(description);
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
@@ -38,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         createNotificationChannel();
 
         Button btn = (Button) findViewById(R.id.btn);
+        date = findViewById(R.id.date);
         btn.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
@@ -47,10 +58,11 @@ public class MainActivity extends AppCompatActivity {
                 //Toast.makeText(getBaseContext(), str, Toast.LENGTH_SHORT).show();
 
                 NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getBaseContext(), CHANNEL_ID)
-                        .setSmallIcon(R.drawable.ic_launcher_background)
-                        .setContentTitle("My notification")
+                        .setSmallIcon(R.drawable.ic_check_box)
+                        .setContentTitle("Poke")
                         .setContentText(str)
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setVibrate(new long[0]);
 
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getBaseContext());
 
@@ -59,5 +71,42 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+
+
+            }
+        });
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+                Date date = new Date(year-1900,month,day);
+                SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+
+                MainActivity.date.setText(formatter.format(date));
+
+
+
+        }
     }
 }
